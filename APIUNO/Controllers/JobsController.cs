52e9e6fef1d809127e2c27bebb0a47f567dffd30
sattara1985 <sattara1985@gmail.com>
2023-02-09 10:52:24 -0500

    SE ELIMINO WAETHER Y SE AGRERGO JOBS CONTROLLER

---
+++ APIUNO/Controllers/JobsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace APIUNO.Controllers
+{
+    [ApiController]
+    [Route(template:"[controller]")]
+    public class JobsController: ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            return  Ok("Hola mundo steven");
+        }
+    }
+}
