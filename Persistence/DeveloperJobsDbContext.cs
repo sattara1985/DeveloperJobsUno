8ae4fc280e3a4354c50729c453cdfa9569837b87
sattara1985 <sattara1985@gmail.com>
2023-02-09 19:57:35 -0500

    Se creo el mapeo

---
+++ Persistence/DeveloperJobsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BusinessDos.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DeveloperJobsDbContext: DbContext
+    {
+        public DeveloperJobsDbContext(DbContextOptions options):base(options) 
+        { 
+            
+        }
+        public DbSet<Job>Jobs { get; set; }
+
+    }
+}
---
+++ Persistence/Maps/JobMap.cs
@@ -0,0 +1,19 @@
+﻿using BusinessDos.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Maps
+{
+    public class JobMap : IEntityTypeConfiguration<Job>
+    { 
+        public void Configure(EntityTypeBuilder<Job>builder)
+        {
+            builder.Property(t => t.JobTitle)
+                
+                .IsRequired();
+        }
+    }
+}
